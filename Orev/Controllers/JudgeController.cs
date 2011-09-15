a31117ac898cc770edd075037420d5a7ca611e68
Itamar Syn-Hershko <itamar@code972.com>
2011-09-15 17:35:24 +0300

    Work on judgments portion of the app

--- Orev/Controllers/JudgeController.cs
+++ Orev/Controllers/JudgeController.cs
@@ -1,20 +1,117 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Orev.Models;
 
 namespace Orev.Controllers
 {
     public class JudgeController : RavenController
     {
-        //
-        // GET: /Judge/
+		[HttpGet]
+		[Authorize]
+		public ActionResult Index(int? topicId, int? corpusId, string lang)
+		{
+			// Go to topic selection if none selected
+			if (topicId == null || topicId <= 0)
+			{
+				if (!string.IsNullOrEmpty(lang) || corpusId > 0)
+					return RedirectToAction("SelectTopic", new {corpusId = corpusId, lang = lang});
+				return RedirectToAction("SelectLanguage");
+			}
 
-        public ActionResult Index()
-        {
-            return View();
-        }
+			// Get the actual topic to get its language identifier
+			var topic = RavenSession.Load<Topic>(topicId.Value);
+			if (topic == null || string.IsNullOrWhiteSpace(topic.Language))
+			{
+				ViewBag.Message = "No such topic exists";
+				return View();
+			}
+			lang = topic.Language;
+			ViewBag.Topic = topic;
 
+			// Load the requested corpus if specified, failsafe below
+			if (corpusId > 0)
+			{
+				var corpus = RavenSession.Load<Corpus>(corpusId.Value);
+				if (corpus != null && lang.Equals(corpus.Language, StringComparison.InvariantCultureIgnoreCase))
+				{
+					ViewBag.Corpus = corpus;
+				}
+			}
+
+			// If no corpus was selected, auto-select one matching the topic's language
+			if (ViewBag.Corpus == null)
+			{
+				var corpus = RavenSession.Query<Corpus>()
+					.Where(x => x.Language == lang)
+					.FirstOrDefault();
+
+				if (corpus == null)
+				{
+					ViewBag.Message = "No corpus was found for the selected language";
+				}
+				else
+				{
+					ViewBag.Corpus = corpus;
+				}
+				// Another option: return RedirectToAction("SelectCorpus", new { lang = lang });
+			}
+
+			return View(topic);
+		}
+
+		[HttpPost]
+		[Authorize]
+		public JsonResult SaveJudgment(string topicId, string corpusId, string docId, Judgment.Verdict verdict)
+		{
+			// TODO: input validation
+
+			var j = new Judgment
+			        	{
+			        		CorpusId = corpusId,
+			        		TopicId = topicId,
+			        		DocumentId = docId,
+			        		UserJudgement = verdict,
+			        		UserId = "users/" + User.Identity.Name
+			        	};
+
+			RavenSession.Store(j);
+			RavenSession.SaveChanges();
+
+			var nextDoc = RavenSession.Query<CorpusDocument>()
+				.Where(x => x.CorpusId == corpusId)
+				.FirstOrDefault();
+		}
+
+    	[HttpGet]
+		[Authorize]
+		public ActionResult SelectLanguage()
+		{
+			var langs = RavenSession.Advanced.DatabaseCommands.GetTerms("CorpusBasicIndex", "Language", null, 50);
+			if (langs.Count() == 0)
+				ViewBag.Message = "No languages were found. Please add a new topic";
+
+			return View(langs);
+		}
+
+		[HttpGet]
+		[Authorize]
+		public ActionResult SelectTopic(int? corpusId, string lang)
+		{
+			Corpus corpus = null;
+			if (corpusId > 0)
+			{
+				corpus = RavenSession.Load<Corpus>(corpusId.Value);
+				if (corpus != null) lang = corpus.Language;
+			}
+
+			var topicsByLang = RavenSession.Query<Topic>()
+				.Where(x => x.Language == lang)
+				.ToArray();
+
+			ViewBag.Corpus = corpus;
+
+			return View(topicsByLang);
+		}
     }
 }
---
+++ Orev/Infrastructure/RavenIndexDefinitions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Orev.Models;
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+
+namespace Orev.Infrastructure
+{
+	public class TopicsBasicIndex : AbstractIndexCreationTask<Topic>
+	{
+		public TopicsBasicIndex()
+		{
+			Map = docs => from doc in docs
+			              select new {doc.Language, doc.UserId, doc.Title, doc.Description, doc.Narrator};
+
+			Indexes = new Dictionary<Expression<Func<Topic, object>>, FieldIndexing>
+			          	{
+			          		{x => x.Title, FieldIndexing.Analyzed},
+			          		{x => x.Description, FieldIndexing.Analyzed},
+			          		{x => x.Narrator, FieldIndexing.Analyzed},
+			          	};
+		}
+	}
+
+	public class CorpusBasicIndex : AbstractIndexCreationTask<Corpus>
+	{
+		public CorpusBasicIndex()
+		{
+			Map = docs => from doc in docs
+			              select new {doc.Language, doc.Name, doc.Description};
+
+			Indexes = new Dictionary<Expression<Func<Corpus, object>>, FieldIndexing>
+			          	{
+			          		{x => x.Name, FieldIndexing.Analyzed},
+			          		{x => x.Description, FieldIndexing.Analyzed},
+			          	};
+		}
+	}
+
+	public class CorpusDocumentsBasicIndex : AbstractIndexCreationTask<CorpusDocument>
+	{
+		public CorpusDocumentsBasicIndex()
+		{
+			Map = docs => from doc in docs
+			              select new {doc.CorpusId, doc.InternalUniqueName};
+		}
+	}
+
+	public class JudgmentsBasicIndex : AbstractIndexCreationTask<Judgment>
+	{
+		public JudgmentsBasicIndex()
+		{
+			Map = docs => from doc in docs
+			              select new {doc.CorpusId, doc.DocumentId, doc.TopicId, doc.UserId, doc.UserJudgement};
+		}
+	}
+
+	public class CorpusDocuments_ByCorpusJudgmentsCount : AbstractIndexCreationTask<Judgment, CorpusDocument>
+	{
+		public CorpusDocuments_ByCorpusJudgmentsCount()
+		{
+			Map = docs => from doc in docs
+						  select new { doc.CorpusId, doc.DocumentId, doc.TopicId, doc.UserId, doc.UserJudgement };
+
+
+		}
+	}
+}
